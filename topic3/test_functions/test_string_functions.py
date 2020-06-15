5f6cce36c060179a5d6ba7b86d9e24277a928563
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-15 16:29:04 -0500

    test passed

--- topic3/test_functions/test_string_functions.py
+++ topic3/test_functions/test_string_functions.py
@@ -1,3 +1,10 @@
+"""
+Program: test_string_functions.py
+Author: Daniel Meeker
+Date: 6/15/2020
+
+This program defines and tests a function to multiply strings.
+"""
 import unittest
 from topic3.more_functions import string_functions as sf
 
