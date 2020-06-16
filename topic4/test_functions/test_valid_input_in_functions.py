42d9467b3165405762a4d5477b0e03a91075e868
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-16 16:25:27 -0500

    Update test_valid_input_in_functions.py
    Added more tests for range validation

--- topic4/test_functions/test_valid_input_in_functions.py
+++ topic4/test_functions/test_valid_input_in_functions.py
@@ -6,6 +6,18 @@ class MyTestCase(unittest.TestCase):
     def test_score_input_test_name(self):
         self.assertEqual("Python: 0", vf.score_input('Python'))
 
+    def test_score_input_test_score_valid(self):
+        self.assertEqual("Python: 98", vf.score_input('Python', 98))
+
+    def test_score_input_test_score_below_range(self):
+        self.assertRaises(ValueError, vf.score_input('Python', -5))
+
+    def test_score_input_test_score_above_range(self):
+        self.assertRaises(ValueError, vf.score_input('Python', 115))
+
+    def test_score_input_test_score_non_numeric(self):
+        self.assertRaises(ValueError, vf.score_input('Python', 'Ten'))
+
 
 if __name__ == '__main__':
     unittest.main()
